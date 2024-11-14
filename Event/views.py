99e505a8f8441a9deef7fd94148f60db4ab1dd26
TouihriMedAmine <92475242+TouihriMedAmine@users.noreply.github.com>
2024-11-14 11:45:52 +0100

    adding loginrequiered

--- Event/views.py
+++ Event/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required  # Import the decorator
 from .models import Event, User_Event
 from .forms import EventForm
 
-# CRUD pour Event
+# CRUD for Event
+@login_required
 def event_list(request):
     events = Event.objects.all()
     return render(request, 'event/event_list.html', {'events': events})
 
+@login_required
 def event_create(request):
     if request.method == 'POST':
         form = EventForm(request.POST)
@@ -17,7 +20,7 @@ def event_create(request):
         form = EventForm()
     return render(request, 'event/event_form.html', {'form': form})
 
-# views.py
+@login_required
 def event_update(request, id):  # Changed pk to id
     event = get_object_or_404(Event, id=id)  # Use id here instead of pk
     if request.method == 'POST':
@@ -29,21 +32,15 @@ def event_update(request, id):  # Changed pk to id
         form = EventForm(instance=event)
     return render(request, 'event/event_form.html', {'form': form})
 
-
-
-# views.py
+@login_required
 def event_delete(request, id):
     event = get_object_or_404(Event, id=id)
-    
-    # Check if the request method is POST (confirm deletion)
     if request.method == 'POST':
         event.delete()
         return redirect('event_list')
-    
-    # If it's a GET request, show the confirmation page
     return render(request, 'event/event_delete.html', {'event': event})
 
-
+@login_required
 def event_detail(request, id):
-    event = get_object_or_404(Event, id=id)  # Récupère l'article avec l'ID spécifié
+    event = get_object_or_404(Event, id=id)  # Retrieve the article with the specified ID
     return render(request, 'event/event_detail.html', {'event': event})
