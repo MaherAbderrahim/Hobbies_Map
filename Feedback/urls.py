9d580fd13c7532ee84010646cc7be7e7f3f80df5
MaherAbderrahim <maher.abderrahim@istic.ucar.tn>
2024-11-28 20:54:50 +0100

    update souleima

--- Feedback/urls.py
+++ Feedback/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'feedback' 
+app_name = 'feedback'  # Namespace is 'feedback'
 
 urlpatterns = [
-    path('', views.feedback_list, name='feedback'),  
+    path('', views.feedback_list, name='feedback'),  # Root URL for the feedback list
     path('create/', views.feedback_create, name='feedback_create'),
     path('update/<int:feedback_id>/', views.feedback_update, name='feedback_update'),  
     path('delete/<int:feedback_id>/', views.feedback_delete, name='feedback_delete'),
